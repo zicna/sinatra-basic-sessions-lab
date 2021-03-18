594ef4b59c47dbbdd74d752682b1812759e40d3a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-17 23:22:00 -0500

    commit tree 6503bc71e138af608b01ac4093ec69a633933c0f

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+       end
+       
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do 
+        @item = params[:item]
+        @session = session
+        @session[:item] = @item
+
+        erb :show
+    end
 end
